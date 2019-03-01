69e6df5504e7601e461b32dc4f14d09cf85528cf
794774196 <794774196@qq.com>
2019-03-02 00:12:05 +0800

    Full struct

--- middle/ContainerStruct.go
+++ middle/ContainerStruct.go
@@ -1,2 +1,26 @@
 package middle
 
+type Container struct {
+	Id string
+	MachineId string
+	App string
+	TimeStamp int
+	CpuRequest int
+	CpuLimit int
+	MemSize float64
+	Status string
+	Usages []CUsage `yaml:"Usages,flow"`
+}
+
+type CUsage struct {
+	MachineId string
+	TimeStamp int
+	CpuPercent int
+	Mpki int
+	Cpi float64
+	MemPercent int
+	MemGps float64
+	DiskIoPercent float64
+	NetIn float64
+	NetOut float64
+}
--- middle/MachineStruct.go
+++ middle/MachineStruct.go
@@ -1,1 +1,23 @@
 package middle
+
+type Machine struct {
+	Id string
+	TimeStamp int
+	Level1 int
+	Level2 int
+	CpuNum int
+	MemSize int
+	Status string
+	Usages []CUsage `yaml:"Usages,flow"`
+}
+
+type MUsage struct {
+	TimeStamp int
+	CpuPercent int
+	MemPercent int
+	MemGps float64
+	Mpki int
+	DiskIoPercent float64
+	NetIn float64
+	NetOut float64
+}
