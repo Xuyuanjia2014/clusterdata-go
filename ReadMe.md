da6e1240ae0c89547e0eb4a2895785e236838ef1
794774196 <794774196@qq.com>
2019-03-03 00:37:09 +0800

    aims.

--- ReadMe.md
+++ ReadMe.md
@@ -9,8 +9,21 @@ This project aims to analyse the cluster data in Alibaba (https://github.com/ali
 
 # Analysis Goals for CSV Batch instances, jobs and tasks
 
+1. A proper format to read all the instances, tasks amd jobs.
+2. Leftover resources for each job and  are they larger engouth?
+3. How there plan resources compared with machines' usage?
+
 # Analysis Goals for CSV online services
+
+1. Instances' resource requirements and their cotainers usages.
+2. Do they have interferences with batch intances?
 
 # Analysis Goals for machine resources
 
+1. The distribution of each machine resources uasage
+2. The patterns of various jobs and applications
+
 # Analysis Goals for integrated instances and containers
+
+1. Can instances perform better under heterogeneous environments?
+2. The affection of online services.
--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	time "time"
 )
 
+var batch_instance ="E:\\benchmark\\alibaba_clusterdata2018\\alibaba_clusterdata_v2018\\batch_instance.csv"
+var batch_task = "E:\\benchmark\\alibaba_clusterdata2018\\alibaba_clusterdata_v2018\\batch_task.csv"
+var container_meta = "E:\\benchmark\\alibaba_clusterdata2018\\alibaba_clusterdata_v2018\\container_meta.csv"
+var container_usage = "E:\\benchmark\\alibaba_clusterdata2018\\alibaba_clusterdata_v2018\\container_usage.csv"
+var machine_meta = "E:\\benchmark\\alibaba_clusterdata2018\\alibaba_clusterdata_v2018\\container_meta.csv"
+var machine_usage = "E:\\benchmark\\alibaba_clusterdata2018\\alibaba_clusterdata_v2018\\container_usage.csv"
+
 func main() {
 	//util.ReadCsv("E:\\benchmark\\alibaba_clusterdata2018\\alibaba_clusterdata_v2018\\batch_task.csv")
 	//util.ReadCsv("E:\\benchmark\\alibaba_clusterdata2018\\alibaba_clusterdata_v2018\\batch_instance.csv")
-	go util.ReadCsv("E:\\benchmark\\alibaba_clusterdata2018\\alibaba_clusterdata_v2018\\batch_instance.csv")
+	go util.ReadCsv(batch_instance)
 	time.Sleep(3 * time.Second)
 	go util.ConvertCsv()
 
